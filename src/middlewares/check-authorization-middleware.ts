2f4ce8f833f166b81b775fdcf09675c67076afcc
VwoLFy <v_wolfy@mail.ru>
2022-10-13 14:28:56 +0300

    Revert "changed Auth"
    This reverts commit 7ef7e70a5ad75908f5dae330d67d4362c9911fd3.

--- src/middlewares/check-authorization-middleware.ts
+++ src/middlewares/check-authorization-middleware.ts
@@ -1,12 +1,11 @@
 import {NextFunction, Request, Response} from "express";
-import {atob, btoa} from "buffer";
+import {atob} from "buffer";
 
 export const checkAuthorizationMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    let authorization = req.header("Authorization");
+    const authorization = req.header("Authorization");
     if (!authorization?.startsWith("Basic")) {
         return res.sendStatus(401);
     }
-    authorization = authorization?.split(" ")[0] +" " + btoa(authorization?.split(" ")[1]);
     const [login, pass] = atob(authorization?.split(" ")[1]).split(":");
     if (login !== "admin" || pass !== "qwerty") {
         return res.sendStatus(401);
