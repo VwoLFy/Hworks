ca7b59d70869a41fb0b77dff916f33bef046c6ca
VwoLFy <v_wolfy@mail.ru>
2022-10-13 14:45:24 +0300

    changed Auth

--- src/middlewares/check-authorization-middleware.ts
+++ src/middlewares/check-authorization-middleware.ts
@@ -3,7 +3,7 @@ import {atob} from "buffer";
 
 export const checkAuthorizationMiddleware = (req: Request, res: Response, next: NextFunction) => {
     const authorization = req.header("Authorization");
-    if (!authorization?.startsWith("Basic")) {
+    if (!authorization?.startsWith("Basic") || authorization?.indexOf(":") > -1) {
         return res.sendStatus(401);
     }
     const [login, pass] = atob(authorization?.split(" ")[1]).split(":");
