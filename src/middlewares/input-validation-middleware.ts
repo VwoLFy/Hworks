62a1917ded1d2aa3268a2087366eaaa6967a8685
VwoLFy <v_wolfy@mail.ru>
2022-10-13 22:01:43 +0300

    changed Auth

--- src/middlewares/input-validation-middleware.ts
+++ src/middlewares/input-validation-middleware.ts
@@ -13,14 +13,23 @@ const apiErrorResult: typeErrorResult = {
 }
 
 export const inputValidationMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    apiErrorResult.errorsMessages.splice(0);
-    const errors = validationResult(req).array({onlyFirstError: true});
-    for (const error of errors) {
-        apiErrorResult.errorsMessages.push({
-            message: error.msg,
-            field: error.param
-        })
-    }
+    //apiErrorResult.errorsMessages.splice(0);
+    const newRes = validationResult.withDefaults({
+        formatter: error => {
+            return {
+                message: error.msg,
+                field: error.param
+            }
+        }
+    })
+    apiErrorResult.errorsMessages = newRes(req).array({onlyFirstError: true})
+    // const errors = validationResult(req).array({onlyFirstError: true});
+    // for (const error of errors) {
+    //     apiErrorResult.errorsMessages.push({
+    //         message: error.msg,
+    //         field: error.param
+    //     })
+    // }
 
     if (apiErrorResult.errorsMessages.length > 0) {
         return res.status(400).json(apiErrorResult);
