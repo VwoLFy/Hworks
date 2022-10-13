4602363f53a646761b1df5b55936b5e4ad4ca87b
VwoLFy <v_wolfy@mail.ru>
2022-10-13 14:23:50 +0300

    changed Auth

--- src/middlewares/check-authorization-middleware.ts
+++ src/middlewares/check-authorization-middleware.ts
@@ -1,11 +1,12 @@
 import {NextFunction, Request, Response} from "express";
-import {atob} from "buffer";
+import {atob, btoa} from "buffer";
 
 export const checkAuthorizationMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    const authorization = req.header("Authorization");
+    let authorization = req.header("Authorization");
     if (!authorization?.startsWith("Basic")) {
         return res.sendStatus(401);
     }
+    authorization = authorization?.split(" ")[0] +" " + btoa(authorization?.split(" ")[1]);
     const [login, pass] = atob(authorization?.split(" ")[1]).split(":");
     if (login !== "admin" || pass !== "qwerty") {
         return res.sendStatus(401);
