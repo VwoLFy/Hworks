9c99913412bb7ef5d11a2d8d8a648a86ec13a98e
VwoLFy <v_wolfy@mail.ru>
2022-10-24 12:03:57 +0300

    added async to blogIdValidation (blogIdValidation)

--- src/routes/posts-router.ts
+++ src/routes/posts-router.ts
@@ -10,9 +10,9 @@ export const postsRouter = Router({});
 const titleValidation = body('title', "'title' must be  a string in range from 1 to 30 symbols").isString().trim().isLength({min: 1, max: 30});
 const shortDescriptionValidation = body('shortDescription', "'shortDescription' must be a string in range from 1 to 100 symbols").isString().trim().isLength({min: 1, max: 100});
 const contentValidation = body('content', "'content' must be a string  in range from 1 to 1000 symbols").isString().trim().isLength({min: 1, max: 1000});
-const blogIdIsExist: CustomValidator = value => {
-    const foundBlog = blogsRepository.findBlog(value)
-    if (!foundBlog)  throw new Error();
+const blogIdIsExist: CustomValidator = async value => {
+    const foundBlog = await blogsRepository.findBlog(value)
+    if (!foundBlog) throw new Error();
     return true;
 
 };
