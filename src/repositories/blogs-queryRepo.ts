e9d44133b4c05045610748784c00494ed6bda7c9
VwoLFy <v_wolfy@mail.ru>
2022-11-01 23:32:01 +0300

    change createBlog return with insertedId method from result Query rename types in Repos change types in db

--- src/repositories/blogs-queryRepo.ts
+++ src/repositories/blogs-queryRepo.ts
@@ -14,9 +14,20 @@ type TypeBlogFromDB = {
     createdAt: string
 };
 
+enum sortDirection {
+    asc = 1,
+    desc = -1
+}
+
 export const blogsQueryRepo = {
-    async findBlogs(): Promise<TypeBlogOutputModel[]> {
-        return (await blogCollection.find({}).toArray()).map( foundBlog => this.blogWithReplaceId(foundBlog) )
+    async findBlogs(searchNameTerm: string | null, pageNumber: number, pageSize: number, sortBy: string, sortDirectionStr: any): Promise<TypeBlogOutputModel[]> {
+        const filter = {};
+        if (searchNameTerm) filter.name = { $regex: searchNameTerm}
+        //const sort = { sortBy: sortDirection[sortDirectionStr]}
+        const sort = {}
+        sort[sortBy] = sortDirection[sortDirectionStr]
+        console.log(searchNameTerm, pageNumber, pageSize, sortBy, sortDirection[sortDirectionStr], filter, sort)
+        return (await blogCollection.find(filter).sort( sort ).toArray()).map( foundBlog => this.blogWithReplaceId(foundBlog) )
     },
     async findBlogById(id: string): Promise<TypeBlogOutputModel | null> {
         const foundBlog = await blogCollection.findOne({_id: new ObjectId(id)})
--- src/routes/blogs-router.ts
+++ src/routes/blogs-router.ts
@@ -1,6 +1,6 @@
 import {Request, Response, Router} from "express";
 import {blogsService} from "../domain/blogs-service";
-import {body} from "express-validator";
+import {body, query} from "express-validator";
 import {inputValidationMiddleware} from "../middlewares/input-validation-middleware";
 import {checkAuthorizationMiddleware} from "../middlewares/check-authorization-middleware";
 import {checkIdValidForMongodb} from "../middlewares/check-id-valid-for-mongodb";
@@ -8,14 +8,52 @@ import {blogsQueryRepo} from "../repositories/blogs-queryRepo";
 
 export const blogsRouter = Router({});
 
+interface ReqQuery {
+    searchNameTerm: string | null
+    pageNumber: number
+    pageSize: number
+    sortBy: string
+    sortDirection: string
+}
+
 const nameValidation = body('name', "'name' must be a string in range from 1 to 15 symbols")
     .isString().trim().isLength({min: 1, max: 15});
 const youtubeUrlValidation = body('youtubeUrl', "'youtubeUrl' must be a string in range from 1 to 100 symbols")
-    .isString().trim().matches("https://([a-zA-Z0-9_-]+.)+[a-zA-Z0-9_-]+(/[a-zA-Z0-9_-]+)*/?$").isLength({min: 1, max: 100});
+    .isString().trim().matches("https://([a-zA-Z0-9_-]+.)+[a-zA-Z0-9_-]+(/[a-zA-Z0-9_-]+)*/?$").isLength({
+        min: 1,
+        max: 100
+    });
 const listOfValidation = [nameValidation, youtubeUrlValidation, inputValidationMiddleware];
+const queryValidation = [
+    query('searchNameTerm').customSanitizer(value => {
+        if (!value) return null;
+        return value
+    }).toLowerCase(),
+    query('pageNumber').customSanitizer(value => {
+        value = Number(value)
+        if (!value) return 1;
+        return value
+    }),
+    query('pageSize').customSanitizer(value => {
+        value = Number(value)
+        if (!value) return 10;
+        return value
+    }),
+    query('sortBy').customSanitizer(value => {
+        const fields = ['id', 'name', 'youtubeUrl', 'createdAt'];
+        if (!value || !fields.includes(value)) return 'createdAt'
+        return value
+    }),
+    query('sortDirection').customSanitizer(value => {
+        const fields = ['asc', 'desc'];
+        if (!value || !fields.includes(value)) return 'desc'
+        return value
+    }),
+]
 
-blogsRouter.get('/', async (req: Request, res: Response) => {
-    res.json(await blogsQueryRepo.findBlogs())
+blogsRouter.get('/', queryValidation, async (req: Request<{}, {}, {}, ReqQuery>, res: Response) => {
+    const {searchNameTerm, pageNumber, pageSize, sortBy, sortDirection} = req.query;
+    res.json(await blogsQueryRepo.findBlogs(searchNameTerm, pageNumber, pageSize, sortBy, sortDirection))
 })
 blogsRouter.get('/:id', checkIdValidForMongodb, async (req: Request, res: Response) => {
     const foundBlog = await blogsQueryRepo.findBlogById(req.params.id);
