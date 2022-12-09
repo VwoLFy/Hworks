bc83e6f6b4bbb8ed9ac95e23a52cbc82c40876a9
VwoLFy <v_wolfy@mail.ru>
2022-12-09 20:31:36 +0300

    correct connect options to DB

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -3,11 +3,10 @@ import mongoose from "mongoose";
 
 const mongoUri = process.env.NODE_ENV === "test" ? settings.MONGO_URI_LOC : settings.MONGO_URI;
 
-const dbName = "Homework"
-
 export async function runDb() {
     try {
-        await mongoose.connect(mongoUri + '/' + dbName)
+        mongoose.set('strictQuery', false)
+        await mongoose.connect(mongoUri)
         console.log("Connected to DB")
     } catch (err) {
         console.log("!!! Can`t connect to DB !!!")
