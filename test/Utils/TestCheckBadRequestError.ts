170ca3259a7cce0c8faaf0603600e73fa6f51e18
VwoLFy <v_wolfy@mail.ru>
2024-04-06 15:32:17 +0300

    refactor(query_repo): - changed id convertors

--- test/Utils/TestCheckBadRequestError.ts
+++ test/Utils/TestCheckBadRequestError.ts
@@ -4,7 +4,7 @@ export const testCheckBadRequestError = (apiErrorResult: BadRequestError, field:
   if (Array.isArray(field)) {
     if (apiErrorResult.errorsMessages.length !== field.length) throw new Error('bad check Error! (quantity of errors)');
     for (const resultElement of apiErrorResult.errorsMessages) {
-      if (!field.includes(resultElement.field)) throw new Error('bad check Error! (not valid error field)');
+      if (!field.includes(resultElement.field)) throw new Error('bad check Error! (not  valid error field)');
     }
     return;
   }
